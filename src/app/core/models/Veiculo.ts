3b5c8deb543e2686035dab6c7f0581a1448db63f
Bruno Staine <brunostaine@hotmail.com>
2023-09-25 17:48:31 -0300

    first commit

---
+++ src/app/core/models/Veiculo.ts
@@ -0,0 +1,11 @@
+export interface Veiculo {
+  id: number;
+  marca: string;
+  nome: string;
+  ano: string;
+  motor: string;
+  combustivel: string;
+  imagem: string; // TODO - Criar um Array
+  cor: string;
+  quilometragem: string;
+}
